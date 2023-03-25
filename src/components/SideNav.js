218ae27ea4a09f5cc11c68f9545599dc6ce5bf59
Prashant Sonwane <prashants0209@gmail.com>
2023-03-25 14:59:59 +0530

    Update SideNav.js

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,11 +1,10 @@
+
 import React from "react";
 import { FaRegCalendarAlt, FaInbox, FaRegCalendar } from "react-icons/fa";
 import "../styles/App.css";
 
 const SideNav = (props) => {
-
-
-
+  
   return (
     <ul className="side-nav">
       <li onClick= {() => {props.change('INBOX')}}>
